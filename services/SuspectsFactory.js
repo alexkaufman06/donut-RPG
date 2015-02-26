c8e4946afac55bfb53589e14696dc5d6b366bc62
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-26 14:34:19 -0800

    Add shuffle functionality to commitCrime so the innocents are in random order

--- services/SuspectsFactory.js
+++ services/SuspectsFactory.js
@@ -1,7 +1,7 @@
 donutRpg.factory("SuspectsFactory", function SuspectsFactory($state) {
   var factory = {};
   factory.suspects = [
-{name: "Tyler", guilty: false, item: "a nose ring with the world's grossest beardhair attached"},
+    {name: "Tyler", guilty: false, item: "a nose ring with the world's grossest beardhair attached"},
     {name: "Gabe", guilty: false, item: 'a chunk of single-origin Panamanian chocolate'},
     {name: "Dustin", guilty: false, item: 'a lost little girl looking for her daddy'},
     {name: "Jay", guilty: false, item: 'an empty bottle of maple syrup'}
@@ -11,15 +11,22 @@ donutRpg.factory("SuspectsFactory", function SuspectsFactory($state) {
   factory.perpetrator = {};
 
   factory.commitCrime = function() {
-    var guiltyIndex = Math.floor(Math.random() * 4);
+    var guiltyIndex = Math.floor(Math.random() * factory.suspects.length);
     factory.suspects[guiltyIndex].guilty = true;
     factory.suspects.forEach(function(suspect) {
+      var holdingArea = [];
       if (suspect.guilty) {
         factory.perpetrator = suspect;
       } else {
         factory.innocents.push(suspect);
       }
     });
+    for (var i = (factory.innocents.length - 1); i > 0; i--) {
+      var j = Math.round(Math.random() * i);
+      var tempSuspect = factory.innocents[j];
+      factory.innocents[j] = factory.innocents[i];
+      factory.innocents[i] = tempSuspect
+    };
   };
 
 
